4fd29036fef607da275d73d0be36b7c0e08151be
danielcoker <danielcoker28@gmail.com>
2018-10-22 16:23:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Testing Using Jasmine
+
+This project demonstrates browser based testing using [Jasmine](https://jasmine.github.io/).
+
+## Steps to run
+
+1. Download the project ZIP from repository.
+2. Unzip files from the zipped folder.
+3. Open `index.html` in a modern browser.
+4. Wait while it gets the feed - when done, test results are displayed at the bottom of the page.
+
+
+## References
+
+1. URL reglular expression - [https://stackoverflow.com/questions/8188645/javascript-regex-to-match-a-url-in-a-field-of-text]
+
+2. [https://devhints.io/jasmine]
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,128 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function() {
+    /* This is our first test suite - a test suite just contains
+    * a related set of tests. This suite is all about the RSS
+    * feeds definitions, the allFeeds variable in our application.
+    */
+    describe('RSS Feeds', function() {
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the
+         * page?
+         */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+
+        /* TODO: Write a test that loops through each feed
+         * in the allFeeds object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
+        it('has URL defined and that URL is not empty', function() {
+            const urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/ // URL pattern
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined(); // expect URL is defined
+                expect(feed.url.length).not.toBe(0); // expect feed URL is not empty
+                expect(feed.url).toMatch(urlPattern); // expect feed URL is actually a URL
+            });
+        });
+        
+
+        /* TODO: Write a test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
+        it('has name defined and name is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined(); // expect feed name to be defined
+                expect(feed.name.length).not.toBe(0); // expect feed name is not empty
+            });
+        })
+    });
+
+
+    /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+
+        /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the
+         * hiding/showing of the menu element.
+         */
+        it('has menu element hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+         /* TODO: Write a test that ensures the menu changes
+          * visibility when the menu icon is clicked. This test
+          * should have two expectations: does the menu display when
+          * clicked and does it hide when clicked again.
+          */
+         it('menu changes visibility when menu icon is clicked', function() {
+
+             /* When menu icon is clicked, 'menu-hidden' class is removed.
+                Expect body class to not have 'menu-hidden' class when clicked
+            */
+            $('a.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+
+            /* When menu icon is clicked, 'menu-hidden' class is add.
+                Expect body class to have 'menu-hidden' class when clicked
+            */
+            $('a.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
+    });
+
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('has at east a single entry present in feed container', function() {
+            expect($('.feed .entry').length).toBeGreaterThan(0); // Expect at least a single element
+        });
+    });
+    
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
+        let initialFeed;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                // Store content of old feed in initialFeed variable
+                initialFeed = $('.feed').html();
+                // Get new feed
+                loadFeed(1, done);
+            });
+        });
+        
+        it('has a new feed content', function() {
+            expect($('.feed').html()).not.toMatch(initialFeed); // expect current latest feed to not match initita feed content
+        });
+    });
+}());
